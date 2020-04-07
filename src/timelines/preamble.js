ce87e1e740493cc5ccb04cc292e111be11499849
fernandogelin <fernando@fernandogelin.com>
2020-04-07 10:45:05 -0400

    fix: move adjust volume screen

--- src/timelines/preamble.js
+++ src/timelines/preamble.js
@@ -3,6 +3,7 @@ import startCode from '../trials/startCode'
 import userId from '../trials/userId'
 import holdUpMarker from '../trials/holdUpMarker'
 import { AT_HOME } from '../config/main'
+import adjustVolume from '../trials/adjustVolume'
 
 console.log('at_home', AT_HOME)
 console.log('env at home', process.env.REACT_APP_AT_HOME)
@@ -11,7 +12,7 @@ const preamble = {
   stimulus: '',
   timeline: (AT_HOME) ?
     [experimentStart(), userId()] :
-    [experimentStart(), userId(), holdUpMarker(), startCode()]
+    [experimentStart(), userId(), holdUpMarker(), adjustVolume(), startCode()]
 }
 
 export default preamble
