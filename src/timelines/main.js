3641b7a21530577e44d366526320307d9258438f
fernandogelin <fernando@fernandogelin.com>
2020-05-15 09:59:08 -0400

    feat: add camera and screen capture

--- src/timelines/main.js
+++ src/timelines/main.js
@@ -5,20 +5,24 @@ import movingDot from '../trials/movingDot'
 import eyesTask from  '../trials/eyesTask'
 import fixation from '../trials/fixation'
 import experimentEnd from '../trials/experimentEnd'
+import camera from '../trials/camera'
+
 const primaryTimeline = [
         preamble,
-        initialInstructions,
+        initialInstructions(),
+        camera(),
         movingDot('left'),
-        movingDot('right'),
-        movingDot('up'),
-        movingDot('down'),
-        instructions(lang.instructions.blink),
-        eyesTask('blink'),
-        instructions(lang.instructions.close_eyes),
-        eyesTask('close'),
-        instructions(lang.instructions.fixate),
-        fixation(91000),
-        experimentEnd(1000)
+        experimentEnd(10000)
+        // movingDot('right'),
+        // movingDot('up'),
+        // movingDot('down'),
+        // instructions(lang.instructions.blink),
+        // eyesTask('blink'),
+        // instructions(lang.instructions.close_eyes),
+        // eyesTask('close'),
+        // instructions(lang.instructions.fixate),
+        // fixation(91000),
+        // experimentEnd(1000)
         ]
 
 
--- src/trials/instructions.js
+++ src/trials/instructions.js
@@ -1,17 +1,18 @@
 import { lang } from '../config/main'
 import { baseStimulus } from '../lib/markup/stimuli'
 
-
-const movingDotInstruction = baseStimulus(`
+const initialInstructions = () => {
+  const stimulus = baseStimulus(`
   <div class='instructions'>
   <h1>${lang.instructions.moving_dot}</h1>
   </div>
   `, true)
-
-const initialInstructions = {
-  type: 'instructions',
-  pages: [movingDotInstruction],
-  show_clickable_nav: true
+  return {
+    type: 'html_keyboard_response',
+    stimulus: stimulus,
+    prompt:  lang.prompt.continue.press,
+    response_ends_trial: true
+  }
 }
 
 const instructions = (language) => {
