ecd84fb1947b93efe474209a25eaea02686cd290
fernandogelin <fernando@fernandogelin.com>
2020-07-20 13:39:13 -0400

    ci: change logic to get sha and ref

--- version.js
+++ version.js
@@ -3,13 +3,23 @@
 const fs = require('fs-extra')
 const execa = require('execa');
 
-const rev = execa.commandSync('git rev-parse HEAD').stdout
-const branch = execa.commandSync('git branch --show-current').stdout  
-const git = {rev, branch}
+let git;
+
+if (process.env.CI) {
+    const sha = process.env.GITHUB_SHA
+    const ref = process.env.GITHUB_REF
+    git = {sha, ref}
+}
+else {
+    const sha = execa.commandSync('git rev-parse HEAD').stdout
+    const ref = execa.commandSync('git branch --show-current').stdout  
+    git = {sha, ref}
+}
+
 
 fs.writeFile('public/config/version.json', JSON.stringify(git))
   .then(() => {
-      console.log(`Saved version file with rev: ${git.rev}, branch: ${git.branch}`);
+      console.log(`Saved version file with rev: ${git.sha}, branch: ${git.ref}`);
   })
   .catch((err) => {
       console.log(err);
