4157ceb8c07b964278034c47cfb99830cfd15b39
fernandogelin <fernando@fernandogelin.com>
2020-07-28 10:15:50 -0400

    fix: clean up scripts, remove event marker message

--- src/trials/holdUpMarker.js
+++ src/trials/holdUpMarker.js
@@ -1,20 +1,17 @@
 import { lang } from '../config/main'
 import { photodiodeGhostBox } from '../lib/markup/photodiode'
-import { baseStimulus } from '../lib/markup/stimuli'
-import eventMarkerMessage from '../lib/markup/eventMarkerMessage'
 
 const holdUpMarker = () => {
-  let stimulus = baseStimulus(`<div><h2 id='usb-alert'></h2></div>`, true) + photodiodeGhostBox()
+  let stimulus = `<div class="beads_container">
+      <div class="instructions">
+        <h2>${lang.instructions.align_photodiode}</h2>
+      </div>
+    </div>` + photodiodeGhostBox()
 
    return {
     type: 'html_button_response',
     stimulus: stimulus,
-    choices: [lang.prompt.continue.button],
-    prompt: `<br><h3>${lang.instructions.align_photodiode}</h3>`,
-    on_load: () => (eventMarkerMessage()
-                      .then(s => document.getElementById('usb-alert')
-                      .innerHTML = s )
-                    )
+    choices: [lang.prompt.continue.button]
   }
 }
 
